a984074b3b374875510836f74e4b772a58130e58
Brian Hicks <brian@brianthicks.com>
2015-12-18 13:43:52 -0600

    cmd: add config parsing

--- cmd/server.go
+++ cmd/server.go
@@ -1,9 +1,11 @@
 package cmd
 
 import (
+	"github.com/asteris-llc/gestalt/store"
 	"github.com/asteris-llc/gestalt/web"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
+	"io/ioutil"
 )
 
 // ServerCmd starts the server
@@ -11,7 +13,19 @@ var ServerCmd = &cobra.Command{
 	Use:   "server",
 	Short: "start the server",
 	RunE: func(cmd *cobra.Command, args []string) error {
-		web.Run(viper.GetString("address"), nil)
+		// read config file
+		configBytes, err := ioutil.ReadFile(viper.GetString("config"))
+		if err != nil {
+			return err
+		}
+
+		config, err := store.FromConfig(configBytes)
+		if err != nil {
+			return err
+		}
+
+		// run server with config file
+		web.Run(viper.GetString("address"), config)
 		return nil
 	},
 }
@@ -19,4 +33,5 @@ var ServerCmd = &cobra.Command{
 // ServerFlags binds flags to the server
 func ServerFlags() {
 	ServerCmd.Flags().String("address", ":3000", "address to serve on")
+	ServerCmd.Flags().String("config", "gestalt.toml", "config file to read for the server")
 }
