2c72f085b21d297e6e803c6b2bfd103883015f82
Brian Hicks <brian@brianthicks.com>
2015-12-21 14:22:37 -0600

    logrus: remove :'(

--- main.go
+++ main.go
@@ -1,26 +1,19 @@
 package main
 
 import (
-	"github.com/Sirupsen/logrus"
 	"github.com/asteris-llc/gestalt/cmd"
 	"github.com/spf13/cobra"
 	"github.com/spf13/pflag"
 	"github.com/spf13/viper"
+	"log"
 	"os"
 )
 
 var rootCmd = &cobra.Command{
 	Use: "gestalt",
-	PersistentPreRun: func(cmd *cobra.Command, args []string) {
-		setupLogging()
-	},
 }
 
 func init() {
-	// root and persistent flags
-	rootCmd.PersistentFlags().String("log-level", "info", "one of debug, info, warn, error, or fatal")
-	rootCmd.PersistentFlags().String("log-format", "text", "output format (text or json)")
-
 	cmd.ServerFlags()
 
 	flagsets := []*pflag.FlagSet{
@@ -29,7 +22,7 @@ func init() {
 	}
 	for _, flagset := range flagsets {
 		if err := viper.BindPFlags(flagset); err != nil {
-			logrus.WithField("error", err).Fatal("could not bind flags")
+			log.Fatal(err)
 		}
 	}
 
